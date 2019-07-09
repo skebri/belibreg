b845b30936a5d0b7053fce63384f81fcecf8c48f
astankovic <aleksa.stankovic@berkeley.edu>
2019-07-09 18:16:46 +0200

    Add basic burger menu.

--- src/Common/Header/Header.jsx
+++ src/Common/Header/Header.jsx
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../../assets/logo.png';
 import { Link } from 'react-router-dom';
 import './Header.scss';
 
-const Header = () => (
-  <div className="header">
-    <div className="logo-area">
-      <Link to="/"><img src={logo} alt="logo"/></Link>
+const Header = () => {
+  const [checked, setChecked] = useState(false);
+
+  const changeChecked = () => { setChecked(!checked); };
+
+  return (
+    <div className="header">
+      <div className="logo-area">
+        <Link to="/"><img src={logo} alt="logo"/></Link>
+      </div>
+
+      <input className="menu-btn" onChange={() => {}} type="checkbox" id="menu-btn" checked={checked} />
+      <label className="menu-icon" htmlFor="menu-btn" onClick={changeChecked}><span className="navicon" /></label>
+
+      <nav>
+        <Link to='/'>Pocetna strana</Link>
+        <Link to='/oprojektu'>O projektu</Link>
+        <Link to='/pStanova/1'>Ponuda stanova</Link>
+        <Link to='/galerija'>Galerija</Link>
+        <Link to='/kontakt'>Kontakt</Link>
+      </nav>
     </div>
-    <nav>
-      <Link to='/'>Pocetna strana</Link>
-      <Link to='/oprojektu'>O projektu</Link>
-      <Link to='/pStanova/1'>Ponuda stanova</Link>
-      <Link to='/galerija'>Galerija</Link>
-      <Link to='/kontakt'>Kontakt</Link>
-    </nav>
-  </div>
-)
+  )
+};
 
 export default Header;
